3e94e75e55e1964dfa282587b858b44ee3523020
Hady Mashhour <hady.mashhour@gmail.com>
2023-09-22 09:57:10 +0400

    added dom content loaded in nav.js

--- week2/nav.js
+++ week2/nav.js
@@ -1,13 +1,15 @@
-const paths = window.location.pathname.split('/');
-let pagePath = paths[paths.length - 1];
-if (!pagePath.includes('.html')) {
-  pagePath = 'index.html';
-}
-const page = pagePath.split('.')[0];
-
-const lis = document.querySelectorAll('nav ul li');
-lis.forEach((nav) => {
-  if (nav.childNodes[0].innerText.toLowerCase() == page.toLowerCase()) {
-    nav.classList.add('current');
+document.addEventListener('DOMContentLoaded', function () {
+  const paths = window.location.pathname.split('/');
+  let pagePath = paths[paths.length - 1];
+  if (!pagePath.includes('.html')) {
+    pagePath = 'index.html';
   }
+  const page = pagePath.split('.')[0];
+
+  const lis = document.querySelectorAll('nav ul li');
+  lis.forEach((nav) => {
+    if (nav.childNodes[0].innerText.toLowerCase() == page.toLowerCase()) {
+      nav.classList.add('current');
+    }
+  });
 });
