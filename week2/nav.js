478c79d016a6a48c32ca71d52d8a88decaf9bac9
Hady Mashhour <hady.mashhour@gmail.com>
2023-09-22 10:11:23 +0400

    fixed a bug with the nav bar selection not working cus index != home

--- week2/nav.js
+++ week2/nav.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   const paths = window.location.pathname.split('/');
-  console.log('paths', paths);
   let pagePath = paths[paths.length - 1];
-  if (!pagePath.includes('.html')) {
-    pagePath = 'index.html';
+  if (!pagePath.includes('.html') || pagePath == 'index.html') {
+    pagePath = 'home.html';
   }
-  console.log('pagePath', pagePath);
 
   const page = pagePath.split('.')[0];
-  console.log('page', page);
 
   const lis = document.querySelectorAll('nav ul li');
-  console.log('lis', lis);
   lis.forEach((nav) => {
-    console.log('nav', nav);
     if (nav.childNodes[0].innerText.toLowerCase() == page.toLowerCase()) {
       nav.childNodes[0].classList.add('current');
-      console.log('adding current to', nav.childNodes[0]);
     }
   });
 });
